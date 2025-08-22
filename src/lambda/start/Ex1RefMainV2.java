c62f667e72f62c3375583404f6e5c7b546763961
류정균 <prank01@naver.com>
2025-08-23 05:40:57 +0900

    test:firt commit

---
+++ src/lambda/start/Ex1RefMainV2.java
@@ -0,0 +1,44 @@
+package lambda.start;
+import lambda.Procedure;
+
+import java.util.Random;
+
+public class Ex1RefMainV2 {
+
+    public static void hello(Procedure procedure) {
+        //변하지 않는 부분
+        long startNs = System.nanoTime();
+
+        //코드 조각 시작
+        procedure.run();
+        //코드 조각 종료
+
+        //변하지 않는 부분
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+
+    public static void main(String[] args) {
+        hello(new Procedure() {
+            @Override
+            public void run() {
+                //코드 조각 시작
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위 = " + randomValue);
+                //코드 조각 종료
+            }
+        });
+
+        hello(new Procedure() {
+            @Override
+            public void run() {
+                //코드 조각 시작
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+                //코드 조각 종료
+            }
+        });
+    }
+}
